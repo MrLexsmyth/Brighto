c798c7549ce88992623707627917ae5e775a9b25
MrLexsmyth <shittuodunayo4@gmail.com>
2026-02-18 14:45:43 +0100

    Optimize blog page and add 404 error page

--- client/components/NotFoundClient.tsx
+++ client/components/NotFoundClient.tsx
@@ -49,11 +49,15 @@ useEffect(() => {
       <div className="relative z-10 max-w-2xl w-full text-center">
         {/* 404 Text */}
         <div className="relative mb-4">
-          {/* <h1 className="text-[160px] sm:text-[200px] font-black leading-none text-transparent bg-clip-text bg-gradient-to-br from-[#004274]/20 to-[#00aeff]/20 select-none">
-            404
-          </h1> */}
-          <div className="absolute inset-0 flex items-center justify-center">
-            {/* Lottie Animation */}
+             <div className="w-48 h-48 sm:w-64 sm:h-64">
+              <DotLottieReact
+  src="/found.json"
+  loop
+  autoplay
+/>
+            </div>
+          {/* <div className="absolute inset-0 flex items-center justify-center">
+            
             <div className="w-48 h-48 sm:w-64 sm:h-64">
               <DotLottieReact
   src="/found.json"
@@ -61,7 +65,7 @@ useEffect(() => {
   autoplay
 />
             </div>
-          </div>
+          </div> */}
         </div>
 
         {/* Message */}
