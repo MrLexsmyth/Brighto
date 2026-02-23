0567e7a0ccb0ef962c596d444a9e9055ca919f02
MrLexsmyth <shittuodunayo4@gmail.com>
2026-02-23 11:25:12 +0100

    update

--- client/src/app/blog/page.tsx
+++ client/src/app/blog/page.tsx
@@ -44,10 +44,10 @@ export default async function BlogPage() {
   const first20Blogs = blogs.slice(0, 10).slice(1);
 
   return (
-    <div className="max-w-7xl mx-auto px-4 py-8 mb-12">
+    <div className="max-w-7xl mx-auto px-4 py-8 mt-12">
       {/* Header */}
-      <div className="text-center mb-12">
-        <h1 className="text-4xl font-bold mb-8">Blog Insights</h1>
+      <div className="text-center mt-12">
+        <h1 className="text-4xl font-bold mb-4">Blog Insights</h1>
         <p className="text-gray-600 text-lg">
           Explore our latest articles and insights on real estate trends.
         </p>
